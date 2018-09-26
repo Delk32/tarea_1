9556c8a8a0f61d4b5b799708943f32d24161daef
Delk32 <43191046+Delk32@users.noreply.github.com>
2018-09-26 20:10:21 -0300

    se crean funciones de los métodos de estimación de la derivada

--- parte1.py
+++ parte1.py
@@ -1,3 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-print(np.finfo(np.float32))
+import math
+n = np.float32(100)
+delta = np.logspace(0, math.pi, n , dtype="float32" )
+
+def fprimah(x, dt):
+    fh = np.float32((np.cos(dt)-np.cos(x-dt))/dt)
+    return
+    
+def fprimah_2(x, dt):
+    fh_2 = np.float32((np.cos(x + dt)-8*np.cos(x + dt)+8*np.cos(x - dt)\
+    -np.cos(x - dt))/(12 * dt))
+    return
+
