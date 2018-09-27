5dff391ca2830e5f977c47c89b04d981d4751713
Delk32 <43191046+Delk32@users.noreply.github.com>
2018-09-26 22:31:04 -0300

    se crean arreglos con valores de las aproximaciones de cada método

--- parte1.py
+++ parte1.py
@@ -1,15 +1,17 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import math
-n = np.float32(100)
-delta = np.logspace(0, math.pi, n , dtype="float32" )
+n = 100
+x = 1.705
+delta = np.logspace(-1, -15, n , dtype="float32" )
 
-def fprimah(x, dt):
-    fh = np.float32((np.cos(dt)-np.cos(x-dt))/dt)
-    return
-    
-def fprimah_2(x, dt):
-    fh_2 = np.float32((np.cos(x + dt)-8*np.cos(x + dt)+8*np.cos(x - dt)\
-    -np.cos(x - dt))/(12 * dt))
-    return
+m_simple = np.arange(0, n, dtype="float32")
+m_h4 = np.arange(0, n , dtype="float32")
+k = 0
+while k < len(delta):
+    dt = delta[k]
+    m_simple[k]= (np.cos(x)-np.cos(x+dt))/dt
+    m_h4[k] = (np.cos(x + 2* dt)-8*np.cos(x + dt)+8*np.cos(x - dt)\
+    -np.cos(x -2* dt))/(12 * dt)
+    k = k+1
 
