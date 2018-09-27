e9896222e02adaeeb06a8f7cf3f294cb654eb4f3
Delk32 <43191046+Delk32@users.noreply.github.com>
2018-09-27 03:09:43 -0300

    grafica diferencia entre los métodos y el valor real

--- parte1.py
+++ parte1.py
@@ -1,9 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import math
-n = 100
+n = 15
 x = 1.705
-delta = np.logspace(-1, -15, n , dtype="float32" )
+delta = np.logspace(-1, -20, n , dtype="float32" )
 
 m_simple = np.arange(0, n, dtype="float32")
 m_h4 = np.arange(0, n , dtype="float32")
@@ -15,3 +15,13 @@ while k < len(delta):
     -np.cos(x -2* dt))/(12 * dt)
     k = k+1
 
+m_simple -= math.sin(1.705)
+m_h4 -= math.sin(1.705)
+plt.plot(delta, np.fabs(m_simple), label="método O(h)")
+plt.plot(delta, np.fabs(m_h4), label="método con O($h^4$)")
+plt.yscale('log')
+plt.xscale('log')
+plt.ylabel("$\\frac{d}{dx}[-cos(1.705)]-sin(1.705)$", fontsize="15")
+plt.xlabel("$\\delta$x", fontsize="15")
+plt.legend()
+plt.show()
